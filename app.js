9459ac83131dfa02ba7e5d2867a25b5d8f966d2e
TheDomesticUser <thedomesticuser@gmail.com>
2019-06-01 14:59:55 -0400

    Book verification and storage
    Adding a function that verifies if the inputs aren't null, and stores the objects in the local storage. 2019-06-01

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ class Book
         this.bookNumberOfPages = bookNumberOfPages;
     }
     
+    storeBookInLocalStorage() {
+        // Store the book object into the local storage
+        addItemToLocalStorageArray('Books', this);
+    }
 }
 
 // Get the button for adding the book in the book list
@@ -25,7 +29,35 @@ function addBookToList(e)
     const bookYearOfRelease = document.querySelector('form input[id=bookYearRelease]').value;
     const bookNumberOfPages = document.querySelector('form input[id=bookNumberOfPages').value;
 
-    
+    // Verify each input has a value
+    if (!bookTitle) {
+        alert('Please input the book title.');
+    } else if (!bookAuthor) {
+        alert('Please input the book author.');
+    } else if (!bookGenre) {
+        alert('Please input the book genre.');
+    } else if (!bookYearOfRelease) {
+        alert('Please input the book year of release.');
+    } else if (!bookNumberOfPages) {
+        alert('Please input the number of pages.');
+    } else {
+        // Make a book object
+        book = new Book(bookTitle, bookAuthor, bookGenre, bookYearOfRelease, bookNumberOfPages);
+        
+        // Store the book into the local storage
+        book.storeBookInLocalStorage();
 
-    console.log(typeof bookTitle);
+        window.alert('Book successfully added!');
+
+        location.reload();
+    }
+}
+
+function addItemToLocalStorageArray(key, item)
+{
+    // Set the variable to the array in the local storage through the key
+    let arrayItem = !localStorage.getItem(key) ? [] : JSON.parse(localStorage.getItem(key));
+
+    arrayItem.push(item);
+    localStorage.setItem(key, JSON.stringify(arrayItem));
 }
